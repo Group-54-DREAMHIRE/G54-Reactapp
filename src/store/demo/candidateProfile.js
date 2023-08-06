f57197aa8e13a2e8e67ede0306f2706c46e8b6fb
unknown <2019cs186@stu.ucsc.lk>
2023-08-06 18:21:25 +0530

    Add company profile

---
+++ src/store/demo/candidateProfile.js
@@ -0,0 +1,19 @@
+
+export const candidateDetails = {
+  name: "Dulanjana Weerasinghe",
+  title: "Software Engineer",
+  minSalary: "$500",
+  maxSalary: "$700",
+  description:
+    "Creative Software builds and manages high-performing software development teams to bring big ideas to life.Whether you want to extend your in-house team or need assistance with developing, quality assuring, or supporting your application, we can help.",
+  about:
+    "Dedicated software engineering teams.We specialise in recruiting, nurturing and managing top talent to work with you seamlessly. With over two decades of experience, we can build a team with the exact skills you need.",
+  images: "",
+  email: "dpsweerasingh98@gmail.com",
+  phone: "0712905022",
+  city: "Matara",
+  address: "Galhena, Bathigama, Dickwella.",
+  facebook: "www.facebook.com",
+  twitter: "www.twitter.com",
+  linkedIn: "www.linkedin.com",
+};
