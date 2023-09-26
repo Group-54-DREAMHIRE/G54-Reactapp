3e429264d7c85f3b4bb7dbc183bcdc44c755e361
WeerasingheDPS <2019cs186@stu.ucsc.lk>
2023-09-26 16:09:19 +0530

    merge dev

--- src/routes/CompanyRoutes.js
+++ src/routes/CompanyRoutes.js
@@ -45,11 +45,8 @@ import EventForm from "../Components/cards/company/EventForm";
 import CompanyPage from '../pages/CompanyPage';
 import CandidatePage from "../pages/CandidatePage";
 import GenarateInterviews from "../pages/company/GenarateInterviews";
-<<<<<<< HEAD
 import EditJobPost from "../pages/company/EditJobPost"
-=======
 import ViewCanResume from "../pages/company/ViewCanResume";
->>>>>>> dulanjana
 export default function CompanyRoutes() {
   return (
     <>
@@ -63,11 +60,8 @@ export default function CompanyRoutes() {
           <Route path="/addjobpost" element={<AddJobPost/>}/>
           <Route path='/jobpost/:id' element={<JobPost/>}/>
 
-<<<<<<< HEAD
           <Route path="/postedjobs/editjobpost/:id" element={<EditJobPost/>} />
-=======
           <Route path="/viewcanresume/:id" element={<ViewCanResume/>}/>
->>>>>>> dulanjana
 
           <Route element={<CandidateResumes/>}>
             <Route path="/pendingresumes/:id" index element={<PendingResume/>} />
