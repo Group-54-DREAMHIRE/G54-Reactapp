c3321dd9b7b08bde2b22f656a97777d91e0de4f7
Lahiru SWampath <2019CS150@stu.ucsc.cmb.lk>
2023-10-27 13:47:56 +0530

    merge Sampath

--- src/Components/cards/candidate/ViewResumeCard.jsx
+++ src/Components/cards/candidate/ViewResumeCard.jsx
@@ -3,19 +3,21 @@ import addDoc from "../../../assets/images/viewCV.png";
 import { useNavigate } from "react-router-dom";
 const { Title, Text, Link } = Typography;
 export default function ViewResumeCard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <>
-      <Row style={{ cursor: "pointer" }} onClick={()=>navigate("/createresume")}>
+      <Row
+        style={{ cursor: "pointer" }}
+        onClick={() => navigate("/createresume")}
+      >
         <Card hoverable>
           <Col span={24}>
             <Image width={100} preview={false} src={addDoc} />
           </Col>
           <Col span={24}>
-            <Row justify='center'>
-            <span>Resume</span>
+            <Row justify="center">
+              <span>Resume</span>
             </Row>
-           
           </Col>
         </Card>
       </Row>
